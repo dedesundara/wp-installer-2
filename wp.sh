0eac30abe5dfeaba9f147b8329c4d1af60e21e3c
Urban Sanden <hej@urre.me>
2013-05-08 10:36:36 +0200

    Install ACF language files and remove hello dolly

--- wp.sh
+++ wp.sh
@@ -55,8 +55,8 @@ mkdir wp-content/uploads
 chmod 755 wp-content/uploads
 
 # Download language files
-wget --quiet http://wpsv.se/fil/sv_SE-3.5.zip
-unzip -q sv_SE-3.5.zip
+wget --quiet http://wpsv.se/fil/sv_SE-3.5.1.zip
+unzip -q sv_SE-3.5.1.zip
 mkdir wp-content/languages
 mv *.mo wp-content/languages/
 mv *.po wp-content/languages/
@@ -83,15 +83,24 @@ unzip -q simple-history.zip
 mv simple-history/ wp-content/plugins/
 wget --quiet http://downloads.wordpress.org/plugin/my-page-order.zip
 unzip -q my-page-order.zip
-mv my-page-order/ wp-content/plugins/
+mv simple-history/ wp-content/plugins/
+wget --quiet http://downloads.wordpress.org/plugin/wordpress-seo.zip
+unzip -q wordpress-seo.zip
+mv wordpress-seo/ wp-content/plugins/
+
+# Install my language file for Advanced custom fields
+git clone https://github.com/urre/ACF-Swedish-Translation.git acf-swe
+mv acf-swe/* wp-content/plugins/advanced-custom-fields/lang
+rm -rf acf-swe
+
 # Delete zip files
 rm -rf *.zip
 
-# Delete plugins Hello dolly and Askismet
-rm -rf wp-content/plugins/hello-dolly
+# Delete plugins Hello dolly
 rm -rf wp-content/plugins/akismet
+rm wp-content/plugins/hello.php
 
-# Get my start settings
+# Get my start settings plugin
 git clone https://github.com/urre/WordPress-Start-Settings.git
 mv WordPress-Start-Settings/ wp-content/plugins
 rm -rf WordPress-Start-Settings
