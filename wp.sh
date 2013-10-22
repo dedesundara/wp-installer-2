399363480cafb9e11a831d545a5a9294bfee4d81
Urban Sanden <hej@urre.me>
2013-10-22 10:24:43 +0200

    Updated language file url and added some plugins

--- wp.sh
+++ wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get latest wp
+# Get latest WordPress, download, untar, move to correct folder, remove install file
 curl -O http://wordpress.org/latest.tar.gz
 tar -zxvf latest.tar.gz
 mv wordpress/* .
@@ -55,8 +55,8 @@ mkdir wp-content/uploads
 chmod 755 wp-content/uploads
 
 # Download language files
-wget --quiet http://wpsv.se/fil/sv_SE-3.5.1.zip
-unzip -q sv_SE-3.5.1.zip
+wget --quiet http://sv.wordpress.org/wordpress-3.6.1-sv_SE.zip
+unzip -q wordpress-3.6.1-sv_SE.zip
 mkdir wp-content/languages
 mv *.mo wp-content/languages/
 mv *.po wp-content/languages/
@@ -69,24 +69,46 @@ sed -i "" "s/define('WPLANG', '')/define('WPLANG', 'sv_SE')/g" wp-config.php
 wget --quiet http://downloads.wordpress.org/plugin/advanced-custom-fields.zip;
 unzip -q  advanced-custom-fields.zip
 mv advanced-custom-fields/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/admin-menu-tree-page-view.zip
 unzip -q admin-menu-tree-page-view.zip
 mv admin-menu-tree-page-view/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/enable-media-replace.zip
 unzip -q enable-media-replace.zip
 mv enable-media-replace/ wp-content/plugins/
+
+wget --quiet http://downloads.wordpress.org/plugin/post-thumbnail-editor.zip
+unzip -q post-thumbnail-editor.zip
+mv post-thumbnail-editor/ wp-content/plugins/
+
+wget --quiet http://downloads.wordpress.org/plugin/search-and-replace.zip
+unzip -q search-and-replace.zip
+mv search-and-replace/ wp-content/plugins/
+
+wget --quiet http://downloads.wordpress.org/plugin/admin-menu-tree-page-view.zip
+unzip -q admin-menu-tree-page-view.zip
+mv admin-menu-tree-page-view/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/regenerate-thumbnails.zip
 unzip -q regenerate-thumbnails.zip
 mv regenerate-thumbnails/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/simple-history.zip
 unzip -q simple-history.zip
 mv simple-history/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/my-page-order.zip
 unzip -q my-page-order.zip
 mv simple-history/ wp-content/plugins/
+
 wget --quiet http://downloads.wordpress.org/plugin/wordpress-seo.zip
 unzip -q wordpress-seo.zip
 mv wordpress-seo/ wp-content/plugins/
+
+wget --quiet http://downloads.wordpress.org/plugin/relevanssi.zip
+unzip -q relevanssi.zip
+mv relevanssi/ wp-content/plugins/
 
 # Install my language file for Advanced custom fields
 git clone https://github.com/urre/ACF-Swedish-Translation.git acf-swe
@@ -97,10 +119,9 @@ rm -rf acf-swe
 rm -rf *.zip
 
 # Delete plugins Hello dolly
-rm -rf wp-content/plugins/akismet
 rm wp-content/plugins/hello.php
 
-# Get my start settings plugin
+# Grab my start settings plugin
 git clone https://github.com/urre/WordPress-Start-Settings.git
 mv WordPress-Start-Settings/ wp-content/plugins
 rm -rf WordPress-Start-Settings
